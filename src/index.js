50e6ef93acafc5a2d2b695cfc5411728774ad20a
kyle <mkitigy@hackle.io>
2022-02-25 13:23:16 +0900

    test: sdk 정보 수정

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import * as Hackle from "@hackler/js-client-sdk";
 
-const hackleClient = Hackle.createInstance("jMsedfy75fRlOyw58NEGuUAlZeihnzYb");
+const hackleClient = Hackle.createInstance("odrWk72cBX0tc2mHi3pHsz9VOPiOfuvQ");
 const button = document.getElementById("test-button");
 
 hackleClient.onReady(function () {
+  const variation = hackleClient.variation(5);
+
   if (variation === "A") {
     button.style.backgroundColor = "blue";
   } else if (variation === "B") {
